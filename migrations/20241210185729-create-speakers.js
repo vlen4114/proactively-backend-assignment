5a382790e69ee8da247a850ae68319db7f2fa06f
Vasan Lennin <113226055+vlen4114@users.noreply.github.com>
2024-12-11 23:41:58 +0530

    Add files via upload

---
+++ migrations/20241210185729-create-speakers.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Speakers", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        unique: true,
+      },
+      expertise: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pricePerSession: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("Speakers");
+  },
+};
